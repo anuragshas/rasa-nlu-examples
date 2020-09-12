c16a8e0693355dfe7ea6a865633553999de2fb31
Anurag Singh <anurag.comm@gmail.com>
2020-09-12 08:48:16 +0530

    added SentencePiece Tokeniser

--- rasa_nlu_examples/tokenizers/sentencepiece_tokenizer.py
+++ rasa_nlu_examples/tokenizers/sentencepiece_tokenizer.py
@@ -55,15 +55,15 @@ class SentencePieceTokenizer(WhitespaceTokenizer):
 
         for token in tokens_in:
             token_start, token_end, token_text = token.start, token.end, token.text
-            # use ConveRT model to tokenize the text
+            # use SentencePiece model to tokenize the text
             split_token_strings = self._tokenize(token_text)
 
             # clean tokens (remove special chars and empty tokens)
             split_token_strings = self._clean_tokens(split_token_strings)
 
-            tokens_out.append(train_utils.align_tokens(
+            tokens_out += train_utils.align_tokens(
                 split_token_strings, token_end, token_start
-                ))
+            )
 
         return tokens_out
 
