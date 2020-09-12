03224ef824a0dbd26c4c3665e64326ab1f2fa62d
Anurag Singh <anurag.comm@gmail.com>
2020-09-12 07:46:55 +0530

    added SentencePiece Tokeniser

--- rasa_nlu_examples/tokenizers/__init__.py
+++ rasa_nlu_examples/tokenizers/__init__.py
@@ -1,4 +1,5 @@
 from rasa_nlu_examples.tokenizers.stanzatokenizer import StanzaTokenizer
 from rasa_nlu_examples.tokenizers.thai_tokenizer import ThaiTokenizer
+from rasa_nlu_examples.tokenizers.sentencepiece_tokenizer import SentencePieceTokenizer
 
-__all__ = ["StanzaTokenizer", "ThaiTokenizer"]
+__all__ = ["StanzaTokenizer", "ThaiTokenizer", "SentencePieceTokenizer"]
