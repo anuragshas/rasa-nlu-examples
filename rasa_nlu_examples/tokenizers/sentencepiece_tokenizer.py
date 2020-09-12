7f63ca11d05a228b2134b14a77fff940b3b0e167
Anurag Singh <anurag.comm@gmail.com>
2020-09-12 08:39:02 +0530

    added SentencePiece Tokeniser

--- rasa_nlu_examples/tokenizers/sentencepiece_tokenizer.py
+++ rasa_nlu_examples/tokenizers/sentencepiece_tokenizer.py
@@ -16,6 +16,8 @@ class SentencePieceTokenizer(WhitespaceTokenizer):
         "intent_tokenization_flag": False,
         # Symbol on which intent should be split
         "intent_split_symbol": "_",
+        # Text will be tokenized with case sensitive as default
+        "case_sensitive": True,
         # specifies the path to a custom SentencePiece model file
         "model_file": None,
     }
